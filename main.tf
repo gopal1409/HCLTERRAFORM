cc0caa9919a406bf3310dd1364e886b2905ee85a
John Cena <John@gmail.com>
2021-04-16 05:13:49 -0600

    iteration

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "gopalinstance" {
-   ami = "ami-013f17f36f8b1fefb"
-   instance_type = "t2.micro"
+   count = 2
+   ami = var.ami 
+   instance_type = var.instance_type 
    vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
    key_name =  "terraform"
    user_data = <<EOF
@@ -10,11 +11,11 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -yq
 /usr/bin/apt-get install apache2 -y
 /usr/sbin/ufw allow in "Apache Full"
-/bin/echo "Hello world " >/var/www/html/index.html
+/bin/echo "Hello world gopal" >/var/www/html/index.html
 instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo $instance_ip >>/var/www/html/index.html
 EOF
    tags = {
-       Name = "gopalinstance"
+       Name = "gopalinstance-${count.index}"
    }
 }
--- securitygroups.tf
+++ securitygroups.tf
@@ -1,13 +1,13 @@
 resource "aws_security_group" "webserver_sg" {
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port = 22
-        to_port = 22
+        from_port = var.ssh_port
+        to_port = var.ssh_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }

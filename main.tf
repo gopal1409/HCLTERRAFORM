2243f40a37dd360507dcf9c3f85a17e8f9aae7a8
John Cena <John@gmail.com>
2021-04-16 03:03:54 -0600

    intial commit

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "myinstance" {
-   ami = ami-05d72852800cbf29e
+   ami = "ami-0742b4e673072066f"
    instance_type = "t2.micro"
-   vpc_security_group_ids = 
-   key_name =  terraform
-
+   vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+   key_name =  "terraform"
    tags = {
        Name = "myfirstinstance"
    }

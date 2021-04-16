59e4dfd7812d9dab64bd822df59da500238d02d1
John Cena <John@gmail.com>
2021-04-16 04:27:21 -0600

    user data

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
 resource "aws_instance" "gopalinstance" {
-   ami = "ami-0742b4e673072066f"
+   ami = "ami-013f17f36f8b1fefb"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
    key_name =  "terraform"
+   user_data = <<EOF
+#!/bin/bash -xe
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+/usr/bin/apt-get update
+DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get upgrade -yq
+/usr/bin/apt-get install apache2 -y
+/usr/sbin/ufw allow in "Apache Full"
+/bin/echo "Hello world " >/var/www/html/index.html
+instance_ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo $instance_ip >>/var/www/html/index.html
+EOF
    tags = {
-       Name = "myfirstinstance"
+       Name = "gopalinstance"
    }
 }
